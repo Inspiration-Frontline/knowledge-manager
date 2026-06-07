e8c87560cfc9f3e4490a019b0cf8cf7ea0735f69
DinoStark <15190304050@163.com>
2026-06-07 23:35:47 +0800

    Initialize project.

---
+++ knowledge-manager-core/src/main/java/ifl/agentbreaker/knowledgemanager/KnowledgeManagerMain.java
@@ -0,0 +1,13 @@
+package ifl.agentbreaker.knowledgemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgeManagerMain
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(KnowledgeManagerMain.class, args);
+    }
+}
